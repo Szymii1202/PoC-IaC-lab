5c2f0544139ab51d91d0752bbbb6738b0f2a1c97
Szymii1202 <szymon_dudziak@icloud.com>
2026-06-29 11:40:41 +0200

    Add resource group definition for dev environment

---
+++ env/dev/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.project_name}-${var.environment}"
+  location = var.location
+
+  tags = var.tags
+}
---
+++ env/dev/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+    type = string
+    description = "Nazwa projektu"
+}
+
+variable "location" {
+    type = string
+    description = "Lokalizacja zasobów"
+}
+
+variable "environment" {
+    type = string
+    description = "Środowisko (dev, test, prod)"
+}
+
+variable "tags" {
+    type = map(string)
+    description = "Tagi dodane do zasobów"
+}
