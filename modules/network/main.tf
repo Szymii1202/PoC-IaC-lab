16b4d08f9d76dcc57412f3e519ea91edc255bb2b
Szymii1202 <szymon_dudziak@icloud.com>
2026-06-30 13:08:17 +0200

    Refactor subnet names

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnets" {
   for_each             = var.subnets
-  name                 = "${each.key}Subnet"
+  name                 = each.key
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = each.value.address_prefix
